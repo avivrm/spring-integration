ba34a79fd3380284b7d1f7dcf38446e5ed76b055
Arpit Aggarwal <aggarwalarpit.89@gmail.com>
2016-03-25 12:17:04 +0530

    first commit

---
+++ src/main/java/com/arpit/spring/integration/contants/ApplicationConstants.java
@@ -0,0 +1,11 @@
+package com.arpit.spring.integration.contants;
+
+public class ApplicationConstants {
+
+	/** Don't let anyone else instantiate this class */
+	private ApplicationConstants() {
+
+	}
+
+	public static final String CSV = "*.csv";
+}
